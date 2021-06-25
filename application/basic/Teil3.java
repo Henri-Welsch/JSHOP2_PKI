799de84eb6ae5757af01d281a384388c76f95d72
MrMuhh <weljo870@outlook.de>
2021-06-25 21:49:37 +0200

    .

--- application/basic/Teil3.java
+++ application/basic/Teil3.java
@@ -3,6 +3,8 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.swing.BorderFactory;
@@ -13,10 +15,11 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Teil3 {
+public class test {
     
     public static void main(String[] args) {    
         JFrame frame = new JFrame("Eingabe");
+        
 
         frame.setSize(350, 270);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,6 +33,7 @@ public class Teil3 {
 
 
     private static void placeComponents(JPanel panel) {
+        Map<String, Map<String, String>> data = readDate();
         panel.setLayout(null);
 
         
@@ -46,7 +50,7 @@ public class Teil3 {
 
 
         // * Start Stand des Kilometerzählers 
-        JLabel truck_Label = new JLabel("Anzahl Trucks XXX ");
+        JLabel truck_Label = new JLabel("Anzahl Trucks " + data.get("TRUCK").size());
         truck_Label.setBounds(10,70,150,25);
         panel.add(truck_Label);
 
@@ -55,14 +59,14 @@ public class Teil3 {
         panel.add(truck_Button);
 
         truck_Button.addActionListener(e -> {
-            getManager("TRUCK");
+            getManager(data.get("TRUCK"), "TRUCK");
             System.out.println("Open truck manager");
         });
         // * Ende Stand des Kilometerzählers 
 
 
         // * Start Stand des Stundenkontos. 
-        JLabel driver_Label = new JLabel("Anzahl Fahrer XXX");
+        JLabel driver_Label = new JLabel("Anzahl Fahrer " + data.get("TRUCK-LICENSE").size());
         driver_Label.setBounds(10,100,150,25);
         panel.add(driver_Label);
 
@@ -71,14 +75,14 @@ public class Teil3 {
         panel.add(driver_Button);
 
         driver_Button.addActionListener(e -> {
-            getManager("Driver Manager");
+            getManager(data.get("TRUCK-LICENSE"),"TRUCK-LICENSE");
             System.out.println("Open driver manager");
         });
         // * Ende Stand des Stundenkontos. 
 
 
         // * Start Hallen
-        JLabel hall_Label = new JLabel("Anzahl Hallen XXX");
+        JLabel hall_Label = new JLabel("Anzahl Hallen " + data.get("IN-CITY").size());
         hall_Label.setBounds(10,130,150,25);
         panel.add(hall_Label);
 
@@ -87,7 +91,7 @@ public class Teil3 {
         panel.add(hall_Button);
 
         hall_Button.addActionListener(e -> {
-            getManager("IN-CITY");
+            getManager(data.get("IN-CITY"), "IN-CITY");
             System.out.println("Open hall manager");
         });
         // * Ende Hallen 
@@ -112,59 +116,70 @@ public class Teil3 {
             System.out.println("Cancel");
         });
         // * Ende Input Button
-
-
     }
 
-    private static void getManager(String manager) {
-        AtomicInteger counter = new AtomicInteger(0);
+    private static void getManager(Map<String, String> data, String manager) {
         AtomicInteger nrCounter = new AtomicInteger(1);
         JFrame newFrame = new JFrame(manager);
 
         JPanel vPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
         
 
+        data.entrySet().stream().forEach(str -> {
+            JPanel hPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+            hPanel.add(Box.createRigidArea(new Dimension(2, 0)));
+        
+            JLabel x = new JLabel("Nr: " + nrCounter.getAndIncrement()); 
+            x.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            x.setPreferredSize(new Dimension(40, 20));
+
+
+            JLabel a = new JLabel(str.getKey()); 
+            a.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            a.setPreferredSize(new Dimension(90, 20));
+
+            JTextField b = new JTextField(str.getValue());
+            b.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            b.setPreferredSize(new Dimension(100, 20));
+
+            JButton c = new JButton("Löschen");
+            c.setPreferredSize(new Dimension(90, 20));
+            hPanel.add(Box.createRigidArea(new Dimension(2, 0)));
+            
+            hPanel.add(x);
+            hPanel.add(a);
+            hPanel.add(b);
+            hPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+            hPanel.add(c);
+            vPanel.add(hPanel);
+        });
+
+            
+        newFrame.add(vPanel);
+        newFrame.setSize(350, 270);
+        //newFrame.pack();          // Important for later
+        newFrame.setVisible(true);
+    }
+
+    
+    private static Map<String, Map<String, String>> readDate() {
+        Map<String, Map<String, String>> data = new HashMap<>();
+
         try (FileReader fileReader = new FileReader("application/basic/problem")) {
             BufferedReader reader = new BufferedReader(fileReader);
 
             reader.lines().filter(str -> !str.isBlank()).map(String::trim)
                 .map(str -> str.replace("(", "")).map(str -> str.replace(")", ""))
-                .map(str -> str.split(" ")).filter(str -> str[0].equals(manager))
-                .forEach(str -> {
-                    JPanel hPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
-                    hPanel.add(Box.createRigidArea(new Dimension(2, 0)));
-                    
-                    JLabel x = new JLabel("Nr: " + nrCounter.getAndIncrement()); 
-                    x.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                    x.setPreferredSize(new Dimension(40, 20));
-
-
-                    JLabel a = new JLabel(str[1]); 
-                    a.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                    a.setPreferredSize(new Dimension(90, 20));
-
-                    JTextField b = new JTextField(str[2]);
-                    b.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                    b.setPreferredSize(new Dimension(100, 20));
-
-                    JButton c = new JButton("Löschen");
-                    c.setPreferredSize(new Dimension(90, 20));
-                    hPanel.add(Box.createRigidArea(new Dimension(2, 0)));
-                    counter.set(counter.get()+20);
-                    
-                    hPanel.add(x);
-                    hPanel.add(a);
-                    hPanel.add(b);
-                    hPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-                    hPanel.add(c);
-                    vPanel.add(hPanel);
-                });
-            
-        } catch (Exception e) {System.out.println(e.getMessage());}
-        
-        newFrame.add(vPanel);
-        newFrame.setSize(350, 270);
-        //newFrame.pack();          // Important for later
-        newFrame.setVisible(true);
+                .map(str -> str.split(" ")).forEach(str -> {
+                        try {
+                            if (data.get(str[0]) == null) 
+                                data.put(str[0], new HashMap<>());
+                            if (data.get(str[0]).get(str[1]) == null)
+                                data.get(str[0]).put(str[1], str[2]);
+                        } catch (Exception e) {}
+                    });
+        } catch (Exception e) { System.out.println(e); }
+        // data.entrySet().stream().forEach(x -> x.getValue().entrySet().stream().forEach(y -> System.out.println(x.getKey() + " " + y.getKey() + " " + y.getValue())));
+        return data;
     }
 }
